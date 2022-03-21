d680178d77016ea7ca7d19077361a2147117a10f
iaspnetcore <290996724@qq.com>
2022-03-22 03:56:16 +0800

    FastTunnel.Server: Add UseUseWindowsService()

--- src/FastTunnel.Server/Program.cs
+++ src/FastTunnel.Server/Program.cs
@@ -1,4 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+
+var webApplicationOptions = new WebApplicationOptions()
+
+{
+    ContentRootPath = AppContext.BaseDirectory,
+    Args = args,
+    ApplicationName = System.Diagnostics.Process.GetCurrentProcess().ProcessName
+};
+
+
+var builder = WebApplication.CreateBuilder(webApplicationOptions);
+
+//come from:https://stackoverflow.com/questions/70571849/host-asp-net-6-in-a-windows-service
+builder.Host.UseWindowsService();
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -21,4 +38,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
 
